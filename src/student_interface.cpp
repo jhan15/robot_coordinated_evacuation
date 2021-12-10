11428b5ef25d58430c4f985619adfa1e45f604b5
Jianming Han <jianminghan15@gmail.com>
2021-12-10 19:08:00 +0100

    Roadmap: setup for roadmap test

--- src/student_interface.cpp
+++ src/student_interface.cpp
@@ -3,6 +3,7 @@
 
 #include <stdexcept>
 #include <sstream>
+  
 namespace student {
 
  void loadImage(cv::Mat& img_out, const std::string& config_folder){  
@@ -32,7 +33,7 @@ namespace student {
   }
 
 
-void unwarp(const cv::Mat& img_in, cv::Mat& img_out, const cv::Mat& transf, 
+  void unwarp(const cv::Mat& img_in, cv::Mat& img_out, const cv::Mat& transf, 
             const std::string& config_folder){
     throw std::logic_error( "STUDENT FUNCTION - UNWRAP - NOT IMPLEMENTED" );   
   }
@@ -45,9 +46,56 @@ void unwarp(const cv::Mat& img_in, cv::Mat& img_out, const cv::Mat& transf,
     throw std::logic_error( "STUDENT FUNCTION - FIND ROBOT - NOT IMPLEMENTED" );    
   }
 
-  bool planPath(const Polygon &borders, const std::vector<Polygon> &obstacle_list, const std::vector<std::pair<int, Polygon>> &victim_list, const Polygon &gate, const float x, const float y, const float theta, Path &path, const std::string &config_folder)
-  {
-    throw std::logic_error( "STUDENT FUNCTION - PLAN PATH - NOT IMPLEMENTED" );
+  bool planPath(const Polygon& borders, const std::vector<Polygon>& obstacle_list,
+                const std::vector<std::pair<int,Polygon>>& victim_list,
+                const Polygon& gate,
+                const float x, const float y, const float theta, Path& path,
+                const std::string& config_folder){
+
+    //throw std::logic_error( "STUDENT FUNCTION - PLAN PATH - NOT IMPLEMENTED" );
+    
+    // Here are the positions of border, gate, obstacles, and robot 
+    std::cout<<"\n>>>> Border postion:"<<std::endl;
+    for (const auto &position : borders)
+    {
+      std::cout<<"x="<<position.x<<" y="<<position.y<<std::endl;
+    }
+
+    std::cout<<"\n>>>> Obstacle postion:"<<std::endl;
+    for (auto &obstacle : obstacle_list)
+    {
+      std::cout<<"Obstacle:"<<std::endl;
+      for (const auto &position : obstacle)
+      {
+        std::cout<<"x="<<position.x<<" y="<<position.y<<std::endl;
+      }
+    }
+
+    std::cout<<"\n>>>> Gate postion:"<<std::endl;
+    for (const auto &position : gate)
+    {
+      std::cout<<"x="<<position.x<<" y="<<position.y<<std::endl;
+    }
+
+    std::cout<<"\n>>>> Robot initial postion:"<<std::endl;
+    std::cout<<"x="<<x<<" y="<<y<<" theta="<<theta<<std::endl;
+
+
+    // You can test the roadmap here --------------
+
+
+
+    // --------------------------------------------
+    
+
+    // A fake path
+    float xc = 0, yc = 1.5, r = 1.4;
+    float ds = 0.05;
+    for (float theta = -M_PI/2, s = 0; theta<(-M_PI/2 + 1.2); theta+=ds/r, s+=ds) {
+      path.points.emplace_back(s, xc+r*std::cos(theta), yc+r*std::sin(theta), theta+M_PI/2, 1./r);    
+    }
+
+    return true; 
   }
 }
 
